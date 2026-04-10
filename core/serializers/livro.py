6180f1cb84da41bb429fbb13234dc8ded37aa4b4
rafael-otto <rafael.august.otto.ifc@gmail.com>
2026-04-10 10:44:01 -0300

    feat: criação de dois serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -3,6 +3,13 @@ from rest_framework.serializers import ModelSerializer
 from core.models import Livro
 
 
+class LivroListRetrieverSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
+
+
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroSerializer
+from core.serializers import LivroListRetrieverSerializer, LivroSerializer
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action in {'list', 'retrieve'}:
+            return LivroListRetrieverSerializer
+        return LivroSerializer
