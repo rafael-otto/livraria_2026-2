b4adb19dd5f59b6038d9e6584763c5e93b165000
rafael-otto <rafael.august.otto.ifc@gmail.com>
2026-05-15 10:39:42 -0300

    feat: inclusão da foto de perfil no usuário

--- core/admin.py
+++ core/admin.py
@@ -54,7 +54,7 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ['email', 'name', 'groups__name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Personal Info'), {'fields': ('name', 'foto')}),  # inclua a foto aqui
         (
             _('Permissions'),
             {
