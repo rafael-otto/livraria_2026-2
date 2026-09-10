2dbeb561a6554c2b4aedb12044fc0280213fd268
rafael-otto <rafael.august.otto.ifc@gmail.com>
2026-09-10 20:15:01 -0300

    feat: Gravação do preço do livro no item da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -15,7 +15,7 @@ from core.models import Compra, ItensCompra
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade')
+        fields = ('livro', 'quantidade', 'preco')
 
     def validate_quantidade(self, quantidade):
         if quantidade <= 0:
@@ -41,17 +41,20 @@ class CompraCreateUpdateSerializer(ModelSerializer):
             itens = validated_data.pop('itens')
             compra = Compra.objects.create(**validated_data)
             for item in itens:
+                item['preco'] = item['livro'].preco
                 ItensCompra.objects.create(compra=compra, **item)
             compra.save()
             return compra
 
         @transaction.atomic
         def update(self, compra, validated_data):
-            itens = validated_data.pop('itens', None)
-            if itens is not None:
+            itens = validated_data.pop('itens')
+            if itens:
                 compra.itens.all().delete()
                 for item in itens:
+                    item['preco'] = item['livro'].preco  # grava o preço histórico
                     ItensCompra.objects.create(compra=compra, **item)
+            compra.save()
             return super().update(compra, validated_data)
 
 
@@ -60,7 +63,7 @@ class ItensCompraListSerializer(ModelSerializer):
 
     class Meta:
         model = ItensCompra
-        fields = ('quantidade', 'livro')
+        fields = ('quantidade', 'livro', 'preco')
         depth = 1
 
 
@@ -85,8 +88,8 @@ class ItensCompraSerializer(ModelSerializer):
 
     total = SerializerMethodField()
 
-    def get_total(self, item):
-        return item.livro.preco * item.quantidade
+    def get_total(self, instance):
+        return instance.quantidade * instance.preco
 
     class Meta:
         model = ItensCompra
