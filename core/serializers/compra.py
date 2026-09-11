9335490eee6463f0039f9b2d87dc8256f90e0ced
rafael-otto <rafael.august.otto.ifc@gmail.com>
2026-09-11 10:37:27 -0300

    feat: adicionando tipo de pagamento à entidade de Compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -102,7 +102,9 @@ class CompraSerializer(ModelSerializer):
     status = CharField(source='get_status_display', read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
     data = DateTimeField(read_only=True)
+    tipo_pagamento = CharField(source='get_tipo_pagamento_display', read_only=True) # novo campo
+
 
     class Meta:
         model = Compra
-        fields = ('id', 'usuario', 'status', 'total', 'data', 'itens')
+        fields = ('id', 'usuario', 'status', 'total', 'tipo_pagamento', 'data', 'itens')
